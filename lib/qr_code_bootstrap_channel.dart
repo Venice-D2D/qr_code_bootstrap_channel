ee668b56622a8aaf5bbde9ad15c69f774d866f6d
Rémy Raes <contact@remyraes.com>
2022-09-16 16:37:45 +0200

    refactor: remove template code

--- lib/qr_code_bootstrap_channel.dart
+++ lib/qr_code_bootstrap_channel.dart
@@ -8,11 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
 
 class QrCodeBootstrapChannel extends BootstrapChannel {
   final BuildContext context;
