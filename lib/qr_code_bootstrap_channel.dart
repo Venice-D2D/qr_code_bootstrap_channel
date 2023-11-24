bc25d5cc4513d34aeb8ed612f4326acab8c61a87
Remy Raes <contact@remyraes.com>
2023-11-24 10:23:09 +0100

    fix: package imports

--- lib/qr_code_bootstrap_channel.dart
+++ lib/qr_code_bootstrap_channel.dart
@@ -1,9 +1,9 @@
 library qr_code_bootstrap_channel;
 
 import 'package:venice_core/channels/abstractions/bootstrap_channel.dart';
-import 'package:venice_core/channels/channel_metadata.dart';
+import 'package:venice_core/metadata/channel_metadata.dart';
 import 'package:venice_core/channels/events/bootstrap_channel_event.dart';
-import 'package:venice_core/file/file_metadata.dart';
+import 'package:venice_core/metadata/file_metadata.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:qr_flutter/qr_flutter.dart';
